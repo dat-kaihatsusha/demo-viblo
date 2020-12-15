dce64db9bef869758f268e7d4c453fcc16b85e52
dat-kaihatsusha <dat-kaihatsusha@gmail.com>
2020-12-15 13:45:30 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  resources :microposts
-  resources :users
+  get 'static_pages/about'
+  # resources :microposts
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  # root 'users#index'
 end
